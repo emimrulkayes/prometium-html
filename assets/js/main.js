86e589e2bb334c5f26fc01dbfa3d4921ce4a4dc8
MD IMRUL KAYES <emimrulkayes@gmail.com>
2020-06-15 02:44:51 +0600

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
      //>=, not <=
-    if (scroll >= 100) {
+    if (scroll >= .1) {
         //clearHeader, not clearheader - caps H
         $(".header-menu").addClass("header-sticky");
     }else {
